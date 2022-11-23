2794bbc8ef13da44b4da67836d2aa06a8d6d01a8
louishrm03 <louisherman31@gmail.com>
2022-11-23 09:39:39 +0100

    création de la vue consulter.html.twig dans le dossier responsable

--- src/Controller/ResponsableController.php
+++ src/Controller/ResponsableController.php
@@ -15,4 +15,23 @@ class ResponsableController extends AbstractController
             'controller_name' => 'ResponsableController',
         ]);
     }
+
+
+// Méthode consulterResponsable, qui permet de retourner les informations d'un responsable
+
+public function consulterResponsable(ManagerRegistry $doctrine, int $id){
+
+    $responsable= $doctrine->getRepository(Responsable::class)->find($id);
+
+    if (!$responsable) {
+        throw $this->createNotFoundException(
+        'Aucun responsable trouvé avec le numéro '.$id
+        );
+    }
+
+    return $this->render('responsable/consulter.html.twig', [
+        'responsable' => $responsable,]);
+
+}
+
 }
