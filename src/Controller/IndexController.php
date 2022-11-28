b7ddff91833dd6c1a3df8e1c0876585a0f9012db
charlottelaurent50 <charlottelau50@gmail.com>
2022-11-28 14:43:17 +0100

    essaie d'integration de liste cours et liste instrument dans la page d'accueil

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,5 +14,15 @@ class IndexController extends AbstractController
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
+
+        
+    }
+
+    public function listerCours(ManagerRegistry $doctrine){
+        $repository = $doctrine->getRepository(Cours::class);
+        $cours = $repository->findAll();
+        return $this->render('index/index.html.twig', [
+            'pCours' => $cours,]);	
+            
     }
 }
