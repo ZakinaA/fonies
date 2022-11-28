c5b5add65589f112524194fbebeca1377287acc0
ThomasGlbrt <guilbertthomas14@gmail.com>
2022-11-28 14:18:12 +0100

    Ajout de consulterInstrument et consulter.html.twig

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Instrument;
+use App\Entity\Accessoire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,5 +25,22 @@ class InstrumentController extends AbstractController
     return $this->render('instrument/lister.html.twig', [
     'pInstruments' => $instruments,]);	
     
+    }
+
+    public function consulterInstruments(managerRegistry $doctrine, int $id){
+        
+        $instrument = $doctrine->getRepository(Instrument::class)->find($id);
+        $accessoire = $doctrine->getRepository(Accessoire::class)->findByInstrument($id);
+        if (!$instrument) {
+            throw $this->createNotFoundException(
+            'Aucun responsable trouvé avec le numéro '.$id
+            );
+        }
+
+        return $this->render('instrument/consulter.html.twig', [
+            'instrument' => $instrument,
+            'accessoire' => $accessoire]);
+
+
     }
 }
