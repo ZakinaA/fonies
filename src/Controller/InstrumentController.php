b47266f049744e6c4df76fab5bf4f2dd2b39b056
ThomasGlbrt <guilbertthomas14@gmail.com>
2022-11-30 11:43:15 +0100

    Ajout du controller pret, de la page consulter et maj du instrumentcontroller

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Instrument;
-use App\Entity\Accessoire;
-use App\Entity\ContratPret;
 use App\Entity\InterPret;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,9 +21,6 @@ class InstrumentController extends AbstractController
     public function consulterInstruments(managerRegistry $doctrine, int $id){
         
         $instrument = $doctrine->getRepository(Instrument::class)->find($id);
-        $accessoire = $doctrine->getRepository(Accessoire::class)->findByInstrument($id);
-        $contratPret = $doctrine->getRepository(ContratPret::class)->findByinstrument($id);
-        $interventionPret = $doctrine->getRepository(InterPret::class)->findByContratPret($contratPret);
         if (!$instrument) {
             throw $this->createNotFoundException(
             'Aucun responsable trouvé avec le numéro '.$id
@@ -33,10 +28,7 @@ class InstrumentController extends AbstractController
         }
 
         return $this->render('instrument/consulter.html.twig', [
-            'instrument' => $instrument,
-            'accessoire' => $accessoire,
-            'interventionPret' => $interventionPret,
-            'contratPret' => $contratPret,]);
+            'instrument' => $instrument,]);
 
 
     }
